68f1d8d95cfa86aa5cdf9a21d61f377fe07b8f21
u-gummy <zoz7353@gmail.com>
2023-12-08 18:39:56 +0900

    :memo: Mutation Type, Query Resolvers

--- server.js
+++ server.js
@@ -1,13 +1,44 @@
 import { ApolloServer, gql } from "apollo-server";
+const tweets = [
+  { id: "1", text: "one" },
+  { id: "2", text: "two" },
+];
 
 const typeDefs = gql`
+  type User {
+    id: ID!
+    userName: String!
+  }
+
+  type Tweet {
+    id: ID!
+    text: String!
+    author: User
+  }
+
   type Query {
-    text: String
-    hello: String
+    allTweets: [Tweet!]!
+    tweet(id: ID!): Tweet
+  }
+
+  type Mutation {
+    postTweet(text: String!, userId: ID!): Tweet!
+    deleteTweet(id: ID!): Boolean!
   }
 `;
 
-const server = new ApolloServer({ typeDefs });
+const resolvers = {
+  Query: {
+    allTweets() {
+      return tweets;
+    },
+    tweet(root, { id }) {
+      return tweets.find((tweet) => tweet.id === id);
+    },
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
 server.listen().then(({ url }) => {
   console.log(`Running on ${url}`);
 });
